3f4a883f2afc5b91ffa1cd40e027f3156ee3a871
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-04-10 11:21:09 +0300

    Starting security service

---
+++ devsearch-projects-ws/src/main/java/devsearch/projects/ws/security/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package devsearch.projects.ws.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+	JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+	// jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new
+	// KeycloakRoleConverter()); // delegate to custom
+	// converter
+
+	http.csrf()
+		.disable() // Disable CSRF is needed for POST requests!!!
+		.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/projects/status")
+		.permitAll()
+		.antMatchers(HttpMethod.GET, "/projects/public/all")
+		.permitAll()
+		.antMatchers(HttpMethod.GET, "/projects/public/project/*")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.oauth2ResourceServer()
+		.jwt()
+		.jwtAuthenticationConverter(jwtAuthenticationConverter); // apply jwt converter;
+
+	http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
