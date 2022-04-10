82f4146819f35706ae55668b313f58814f02cd36
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-04-10 11:43:14 +0300

    Adding DTOs

---
+++ devsearch-projects-ws/src/main/java/devsearch/projects/ws/shared/dto/ProjectDto.java
@@ -0,0 +1,106 @@
+package devsearch.projects.ws.shared.dto;
+
+import java.util.List;
+
+public class ProjectDto {
+
+    private String projectId;
+
+    private String name;
+
+    private String author;
+
+    private String about;
+
+    private String sourceCode;
+
+    private String developerId;
+
+    private Integer possitiveFeedback;
+
+    private Integer negativeFeedback;
+
+    private List<TagDto> tags;
+
+    private List<CommentDto> comments;
+
+    public String getProjectId() {
+	return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+	this.projectId = projectId;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public String getAuthor() {
+	return author;
+    }
+
+    public void setAuthor(String author) {
+	this.author = author;
+    }
+
+    public String getAbout() {
+	return about;
+    }
+
+    public void setAbout(String about) {
+	this.about = about;
+    }
+
+    public String getSourceCode() {
+	return sourceCode;
+    }
+
+    public void setSourceCode(String sourceCode) {
+	this.sourceCode = sourceCode;
+    }
+
+    public String getDeveloperId() {
+	return developerId;
+    }
+
+    public void setDeveloperId(String developerId) {
+	this.developerId = developerId;
+    }
+
+    public Integer getPossitiveFeedback() {
+	return possitiveFeedback;
+    }
+
+    public void setPossitiveFeedback(Integer possitiveFeedback) {
+	this.possitiveFeedback = possitiveFeedback;
+    }
+
+    public Integer getNegativeFeedback() {
+	return negativeFeedback;
+    }
+
+    public void setNegativeFeedback(Integer negativeFeedback) {
+	this.negativeFeedback = negativeFeedback;
+    }
+
+    public List<TagDto> getTags() {
+	return tags;
+    }
+
+    public void setTags(List<TagDto> tags) {
+	this.tags = tags;
+    }
+
+    public List<CommentDto> getComments() {
+	return comments;
+    }
+
+    public void setComments(List<CommentDto> comments) {
+	this.comments = comments;
+    }
+}
---
+++ devsearch-projects-ws/src/main/java/devsearch/projects/ws/shared/dto/TagDto.java
@@ -0,0 +1,24 @@
+package devsearch.projects.ws.shared.dto;
+
+public class TagDto {
+
+    private String tagId;
+
+    private String name;
+
+    public String getTagId() {
+	return tagId;
+    }
+
+    public void setTagId(String tagId) {
+	this.tagId = tagId;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+}
