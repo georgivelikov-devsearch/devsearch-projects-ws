ef0e59b507b162fdbcc59b57f3f3faa6fddd9824
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-04-10 15:35:22 +0300

    Starting Project service

---
+++ devsearch-projects-ws/src/main/java/devsearch/projects/ws/service/ProjectService.java
@@ -0,0 +1,23 @@
+package devsearch.projects.ws.service;
+
+import java.util.List;
+
+import devsearch.common.exception.DevsearchApiException;
+import devsearch.projects.ws.shared.dto.ProjectDto;
+import devsearch.projects.ws.shared.dto.ProjectListDto;
+
+public interface ProjectService {
+
+    public ProjectDto getProjectByProjectId(String projectId);
+
+    public ProjectDto createProject(ProjectDto projectDto) throws DevsearchApiException;
+
+    public ProjectDto updateProject(ProjectDto projectDto) throws DevsearchApiException;
+
+    public void deleteProject(String projectId) throws DevsearchApiException;
+
+    public List<ProjectDto> getProjectsForDeveloper(String developerId);
+
+    public ProjectListDto getAllProjects(int page, int limit, String searchText) throws DevsearchApiException;
+
+}
---
+++ devsearch-projects-ws/src/main/java/devsearch/projects/ws/shared/dto/ProjectListDto.java
@@ -0,0 +1,25 @@
+package devsearch.projects.ws.shared.dto;
+
+import java.util.Collection;
+
+public class ProjectListDto {
+
+    private int totalPages;
+    private Collection<ProjectDto> projects;
+
+    public int getTotalPages() {
+	return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+	this.totalPages = totalPages;
+    }
+
+    public Collection<ProjectDto> getProjects() {
+	return projects;
+    }
+
+    public void gsetProjects(Collection<ProjectDto> projects) {
+	this.projects = projects;
+    }
+}
